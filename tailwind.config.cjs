46bdb1cfa3a711b072cecbb7ac7fe5bd43622574
JK <vandung31141@gmail.com>
2022-10-24 17:10:50 +0700

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}','./index.html',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
