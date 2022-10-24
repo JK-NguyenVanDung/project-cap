03cee059877d7d15eed15e1cc5953b3fb6a859d4
JK <vandung31141@gmail.com>
2022-10-24 17:23:40 +0700

    add folders

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-function Header() {
-  return (
-    <>
-      <div className="fixed top-0">
-        <div className="container flex flex-col">
-          <div className="logo px-2"></div>
-          <div className="menu px-3"></div>
-          <div className="login px-2"></div>
-        </div>
-      </div>
-    </>
-  );
-}
 function App() {
   const [count, setCount] = useState(0);
 
---
+++ src/pages/landing page/index.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+function Header() {
+  return (
+    <>
+      <div className="fixed top-0">
+        <div className="container flex flex-col">
+          <div className="logo px-2"></div>
+          <div className="menu px-3"></div>
+          <div className="login px-2"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+const LandingPage = () => {
+  return <></>;
+};
+export default LandingPage;
