7c3054f47bd748df475fd02e5926ad3789ed0aee
nguyen-dzu <j.bnguyendzu@gmail.com>
2022-11-02 11:45:29 +0700

    feat menu header

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,24 +1,24 @@
+import { Button, Input } from '@material-tailwind/react'
 import React, { useCallback } from 'react'
 import { loadFull } from 'tsparticles'
 import Particle from '../../components/Particles/Particle'
-import Button from '../../components/sharedComponents/Button'
 export default function Login() {
   const submit = () => {
     console.log('login')
   }
   return (
     <div className="bg-slate-500">
-      <main className="fixed w-full h-full top-0 place-content-center items-center flex">
-        <form className="w-1/5 h-1/2 flex flex-col bg-slate-50">
-          <div className="md:flex md:items-center mb-6">
+      <main className="fixed w-full h-full top-0 place-content-center items-center flex bg-[#252b42]">
+        <form className="w-1/5 h-1/2 flex flex-col bg-slate-50 bg-white ">
+          <div className="flex flex-col items-start  mb-6">
             <label>email</label>
-            <input />
+            <Input />
           </div>
-          <div className="md:flex md:items-center mb-6">
+          <div className="flex flex-col items-start  mb-6">
             <label>email</label>
-            <input />
+            <Input />
           </div>
-          <div className="md:flex md:items-center">
+          <div className="flex flex-col items-start ">
             <div className="md:w">
               <Button onClick={submit}>Login</Button>
             </div>
