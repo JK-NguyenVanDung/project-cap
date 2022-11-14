ea06eb254805f25a8e204d6685522a973ddc94c5
JK <vandung31141@gmail.com>
2022-11-14 19:34:29 +0700

    fix login config

--- src/pages/authentication/loginconfig.ts
+++ src/pages/authentication/loginconfig.ts
@@ -2,15 +2,14 @@ import { Configuration, PopupRequest } from '@azure/msal-browser';
 
 // Config object to be passed to Msal on creation
 
-const baseUrl = location.protocol + '//' + location.host + '/';
+// const baseUrl = location.protocol + '//' + location.host + '/';
 
-console.log(baseUrl);
 export const msalConfig: Configuration = {
   auth: {
     clientId: '69720f00-5f88-4633-88d3-f3d9a3c9a7b2',
     authority: 'https://login.microsoftonline.com/common',
-    redirectUri: baseUrl,
-    postLogoutRedirectUri: baseUrl,
+    redirectUri: 'http://localhost:5173/',
+    postLogoutRedirectUri: '/',
   },
 };
 
