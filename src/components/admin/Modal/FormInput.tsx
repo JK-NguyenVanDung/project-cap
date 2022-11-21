73833bf482855edfd9de67c40b909ff6ff437f05
JK <vandung31141@gmail.com>
2022-11-21 16:14:57 +0700

    fix FormInput

--- src/components/admin/Modal/FormInput.tsx
+++ src/components/admin/Modal/FormInput.tsx
@@ -47,15 +47,18 @@ export default function FormInput({
         break;
       case 'upload':
         cp = <Uploader></Uploader>;
+        break;
+
       case 'textArea':
         cp = (
           <TextArea
             className="text-black font-customFont  font-bold min-w-[20rem] mt-4 bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-2.5 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             rows={4}
-            placeholder="Nhập miêu tả"
+            placeholder={`Nhập ${label}`}
             maxLength={6}
           />
         );
+        break;
 
       default:
         cp = (
