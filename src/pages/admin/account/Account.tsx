702f1a6b715e800a65be1e3c14b9fb6b8a9f9e65
JK <vandung31141@gmail.com>
2022-12-08 11:33:55 +0700

    fix delete first question

--- src/pages/admin/account/Account.tsx
+++ src/pages/admin/account/Account.tsx
@@ -346,6 +346,7 @@ export default function Account() {
       />
       {showDetail ? (
         <DetailAccount
+          role={role}
           item={detail}
           setItem={setDetail}
           visible={showDetail}
