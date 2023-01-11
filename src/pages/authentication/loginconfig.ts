2e17838ef7d5a82b835225ed3c8a8218ea5457ec
JK <vandung31141@gmail.com>
2023-01-11 19:02:25 +0700

    reconfig

--- src/pages/authentication/loginconfig.ts
+++ src/pages/authentication/loginconfig.ts
@@ -14,8 +14,10 @@ export const msalConfig: any = {
   auth: {
     clientId: '70f575bd-03e9-41fb-8020-9b073facc3f9',
     authority: 'https://login.microsoftonline.com/common',
-    redirectUri: 'https://cntttest.vanlanguni.edu.vn:18081/CP25Team02/',
+    redirectUri: 'http://localhost:5173/',
   },
+
+  // http://localhost:5173/
   //    redirectUri: 'https://cntttest.vanlanguni.edu.vn:18081/SEP25Team17/',
 
   cache: {
