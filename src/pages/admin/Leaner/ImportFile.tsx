a69d3dfefc2f2cb3688f73d696ccfb07c5dd7e1f
Nguyễn Hoàng Vũ <81034816+nguyen-dzu@users.noreply.github.com>
2023-01-16 10:08:21 +0700

    Kyran nguyen (#108)
    * feat: leaner, uppdate login
    
    * feat
    
    * feat: leaner
    
    * fix
    
    * fix: application
    
    * fix: application
    
    * fixx login
    
    * fix login
    
    * fix: Login
    
    * temp fix login
    
    * feat: remove leaner
    
    * application
    
    * fix
    
    * feat: My program
    
    * fix
    
    * fix: import file
    
    Co-authored-by: Nguyen Kyran <8530705-nguyen-dzu@users.noreply.gitlab.com>
    Co-authored-by: JK <vandung31141@gmail.com>

--- src/pages/admin/Leaner/ImportFile.tsx
+++ src/pages/admin/Leaner/ImportFile.tsx
@@ -3,29 +3,41 @@ import React, { useState, useEffect } from 'react';
 import apiService from '../../../api/apiService';
 import CustomModal from '../../../components/admin/Modal/Modal';
 import Input from '../../../components/sharedComponents/Input';
+import { Image } from 'antd';
 import * as XLSX from 'xlsx';
+import TableConfig from '../../../components/admin/Table/Table';
+import { IProgramItem } from '../../../Type';
+import Validate from '../../../config/Validate';
 export default function ImportFile({
   showModal,
   setShowModal,
   loading,
   setLoading,
+  program,
 }: {
   loading: boolean;
   setLoading: (loading: boolean) => void;
   showModal: boolean;
   setShowModal: (showModal: boolean) => void;
+  program: IProgramItem;
 }) {
   const [form] = Form.useForm();
-  const [listEmail, setListEmail] = useState([]);
+  const [listEmail, setListEmail] = useState();
   const handleOk = async () => {
     form
       .validateFields()
-      .then(async (values) => {
-        const data = apiService.importFileLeaner(values);
+      .then(async () => {
+        const values = {
+          programId: program.programId,
+          emails: listEmail,
+        };
+        const data: any = apiService.importFileLeaner(values);
         setLoading(true);
         if (data) {
           setLoading(false);
           notification.success({ message: 'Thêm tập tin thành công' });
+        } else {
+          notification.success({ message: 'Thêm tập tin không thành công' });
         }
         setShowModal(false);
         form.resetFields();
@@ -34,8 +46,8 @@ export default function ImportFile({
       .catch((info) => {});
   };
   const handelReadFile = (value: any) => {
-    const readFileExcel = async () => {
-      const file = value.target.files[0];
+    const file = value.target.files[0];
+    const readFileExcel = new Promise((resolve, reject) => {
       const fileReader = new FileReader();
       fileReader.readAsArrayBuffer(file);
       fileReader.onload = (e) => {
@@ -44,16 +56,70 @@ export default function ImportFile({
         const wsName = workbook.SheetNames[0];
         const ws = workbook.Sheets[wsName];
         const data = XLSX.utils.sheet_to_json(ws);
-        data.map((item: any, index) => {
-          console.log(item.Email);
-        });
+        resolve(data);
       };
-    };
-    readFileExcel();
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+    readFileExcel.then((data: any) => {
+      data &&
+        setListEmail(
+          data.map((item: any) => {
+            const email = item.Email || item.email;
+            if (
+              !email
+                .toString()
+                .match(
+                  /.(?!.*([(),.#/-])\1)*\@vlu.edu.vn$|(?!.*([(),.#/-])\1)*\@vanlanguni.vn$/,
+                )
+            ) {
+              notification.error({ message: 'Email không đúng định dạng' });
+            } else {
+              return email;
+            }
+          }),
+        );
+    });
   };
+  const dataTable = [
+    {
+      key: '1',
+      email: 'demo@vanlanguni.com',
+    },
+    {
+      key: '2',
+      email: 'demo_2@vanlanguni.com',
+    },
+  ];
+  const column = [
+    {
+      title: 'STT',
+      dataIndex: 'key',
+      key: 'key',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+  ];
   const FormItem = () => {
     return (
       <>
+        <p>
+          <span className="text-error">*</span> email phải có thật, định dạng
+          @vanlanguni.vn
+        </p>
+        <p>
+          <span className="text-error">*</span> Ví Dụ Cho Cho Excel
+        </p>
+        <TableConfig
+          panigation={false}
+          search={false}
+          data={dataTable}
+          columns={column}
+        />
         <Input type="file" onChange={(value: any) => handelReadFile(value)} />
       </>
     );
