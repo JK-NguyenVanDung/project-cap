242c2e4d6869986601975dac8c84a517a846b2f5
JK <vandung31141@gmail.com>
2023-02-20 14:43:21 +0700

    reconfig QR scan

--- src/pages/admin/Attendances/TickAttendance.tsx
+++ src/pages/admin/Attendances/TickAttendance.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form, Modal, notification } from 'antd';
 import FormInput from '../../../components/admin/Modal/FormInput';
 import { QrReader } from 'react-qr-reader';
@@ -20,7 +20,7 @@ export default function TickAttendance({
   setVisible: (visible: boolean) => void;
 }) {
   const [form] = Form.useForm();
-  const [dataQrCode, setDataQrCode]: any = useState('Chưa có kết quả');
+  const [dataQrCode, setDataQrCode]: any = useState('');
   const [confirmLoading, setConfirmLoading] = useState(false);
   const items: any['items'] = [
     {
@@ -101,17 +101,21 @@ export default function TickAttendance({
     return (
       <>
         <QrReader
+          scanDelay={1500}
           onResult={(result: any, error: any) => {
             if (!!result) {
               // notification.success({ message: result?.text });
+              if (result && dataQrCode == '') {
+                setDataQrCode(result?.text);
+              }
+              //   setInterval(async () => {
 
-              setDataQrCode(result?.text);
-              handleQr(result?.text);
+              // }, 3000);
             }
           }}
           constraints={undefined}
         />
-        {dataQrCode ? <p>Kết Quả Mã QR: {dataQrCode}</p> : null}
+        {/* {dataQrCode ? <p>Kết Quả Mã QR: {dataQrCode}</p> : null} */}
         <div className=" mt-4 mb-4 flex flex-row justify-evenly w-full">
           <CustomButton
             size="md"
@@ -135,30 +139,39 @@ export default function TickAttendance({
       </>
     );
   }
-  const handleQr = (result: string) => {
-    form.validateFields().then(async (values) => {
-      try {
-        setConfirmLoading(true);
+  useEffect(() => {
+    let time = setTimeout(async () => {
+      handleQr();
+    }, 2000);
+    return () => {
+      clearTimeout(time);
+    };
+  }, [dataQrCode !== '']);
+  const handleQr = async () => {
+    try {
+      setConfirmLoading(true);
 
-        if (result) {
-          const params = {
-            code: result,
-            attendanceId: item.attendance.id,
-          };
-          await apiService.AttdendanceCode(params);
-          notification.success({ message: 'Điểm danh thành công' });
-        }
-        // setVisible(false);
-        // setConfirmLoading(false);
-        // setDataQrCode('');
-      } catch (error) {
-        setVisible(false);
-        setConfirmLoading(false);
-        setDataQrCode('');
-        notification.error({ message: 'Điểm danh không thành công' });
+      if (dataQrCode) {
+        const params = {
+          code: dataQrCode,
+          attendanceId: item.attendance.id,
+        };
+
+        await apiService.AttdendanceCode(params);
+        notification.success({
+          message: 'Điểm danh thành công: ' + dataQrCode,
+        });
       }
-      form.resetFields();
-    });
+      // setVisible(false);
+      // console.count('1');
+      setConfirmLoading(false);
+      setDataQrCode('');
+    } catch (error) {
+      // setVisible(false);
+      setConfirmLoading(false);
+      setDataQrCode('');
+      notification.error({ message: 'Điểm danh không thành công' });
+    }
   };
   const handleOk = () => {
     form.validateFields().then(async (values) => {
