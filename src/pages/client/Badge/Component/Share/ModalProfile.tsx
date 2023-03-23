a44e8ed9c23dc6b87848c432143e8517ef4ee8e0
Nguyen Kyran <8530705-nguyen-dzu@users.noreply.gitlab.com>
2023-03-23 19:20:07 +0700

    fix

--- src/pages/client/Badge/Component/Share/ModalProfile.tsx
+++ src/pages/client/Badge/Component/Share/ModalProfile.tsx
@@ -14,222 +14,254 @@ import { API_URL } from '../../../../../api/api';
 
 export default function ModalProfile({
   open,
+  item,
+  setItem,
   setOpen,
   loadingConfirm,
   setLoadingConfirm,
 }: {
   open: boolean;
+  item: any;
+  setItem: any;
+
   setOpen: any;
   loadingConfirm: boolean;
   setLoadingConfirm: any;
 }) {
-  const item = useAppSelector((state) => state.auth.info);
   const [form] = Form.useForm();
-  // const [dataFct, setDataFct]: any = useState([]);
-  // const [positons, setPositons]: any = useState([]);
-  // const [loading, setLoading] = useState(false);
-  // const [infoImage, setInfoImage] = useState(null);
-  // const inputRef = useRef(null);
-  // const formData = new FormData();
-  // const dispatch = useAppDispatch();
+  const [dataFct, setDataFct]: any = useState([]);
+  const [positons, setPositons]: any = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [infoImage, setInfoImage] = useState(null);
+  const inputRef = useRef(null);
+  const formData = new FormData();
+  const [URLImage, setURLImage] = useState(null);
+  const dispatch = useAppDispatch();
+  const handleChange = (info: any) => {
+    const file = info.target.files[0];
+    setLoading(true);
+    if (file) {
+      setURLImage(URL.createObjectURL(file));
+      setInfoImage(file);
+    }
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
+  const handelOk = () => {
+    form.validateFields().then(async (values) => {
+      setLoadingConfirm(true);
+      formData.append(
+        'Address',
+        values.address ? values.address : item.address,
+      );
+      formData.append(
+        'PhoneNumber',
+        values.phoneNumber ? values.phoneNumber : item.phoneNumber,
+      );
+      formData.append('code', values.code ? values.code : item.code);
+      formData.append(
+        'PositionId',
+        values.positionId ? values.positionId : item.positionId,
+      );
+      formData.append(
+        'FacultyId',
+        values.facultyId ? values.facultyId : item.facultyId,
+      );
+      formData.append(
+        'FullName',
+        values.fullName ? values.fullName : item.fullName,
+      );
+      formData.append(
+        'Avatar',
+        infoImage ? infoImage : `${API_URL}/images/${item.avatar}`,
+      );
+      formData.append('Code', values.code ? values.code.file : item.code);
 
-  // const handleChange = (info: any) => {
-  //   const file = info.target.files[0];
-  //   console.log(URL.createObjectURL(file));
-  // };
-  // const handelOk = () => {
-  //   form.validateFields().then(async (values) => {
-  //     setLoadingConfirm(true);
-  //     formData.append(
-  //       'Address',
-  //       values.address ? values.address : item.address,
-  //     );
-  //     formData.append(
-  //       'PhoneNumber',
-  //       values.phoneNumber ? values.phoneNumber : item.phoneNumber,
-  //     );
-  //     formData.append('code', values.code ? values.code : item.code);
-  //     formData.append(
-  //       'PositionId',
-  //       values.positionId ? values.positionId : item.positionId,
-  //     );
-  //     formData.append(
-  //       'FacultyId',
-  //       values.facultyId ? values.facultyId : item.facultyId,
-  //     );
-  //     formData.append(
-  //       'FullName',
-  //       values.fullName ? values.fullName : item.fullName,
-  //     );
-  //     formData.append(
-  //       'Avatar',
-  //       values.avatar ? values.avatar.file : `${API_URL}/images/${item.avatar}`,
-  //     );
-  //     formData.append('Code', values.code ? values.code.file : item.code);
+      try {
+        const data: any = await apiService.infoAccount(formData);
+        if (data) {
+          setOpen(false);
+          setLoadingConfirm(false);
+          notification.success({ message: 'Cập Nhật Thông Tin Thành Công' });
+          form.resetFields();
+          dispatch(actions.authActions.setInfo(data));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
 
-  //     try {
-  //       const data: any = await apiService.infoAccount(formData);
-  //       if (data) {
-  //         setOpen(false);
-  //         setLoadingConfirm(false);
-  //         notification.success({ message: 'Cập Nhật Thông Tin Thành Công' });
-  //         form.resetFields();
-  //         dispatch(actions.authActions.setInfo(data));
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   });
-  // };
+  useEffect(() => {
+    getPositions();
+    getFacuties();
+    setURLImage(null);
+    setInfoImage(null);
+  }, []);
 
-  // useEffect(() => {
-  //   getPositions();
-  //   getFacuties();
-  // }, []);
-
-  // const getPositions = async () => {
-  //   const reponse: any = await apiService.getPositions();
-  //   if (reponse) {
-  //     setPositons(reponse);
-  //   }
-  // };
-  // const getFacuties = async () => {
-  //   const reponse: any = await apiService.getFaculties();
-  //   if (reponse) {
-  //     setDataFct(reponse);
-  //   }
-  // };
+  const getPositions = async () => {
+    const reponse: any = await apiService.getPositions();
+    if (reponse) {
+      setPositons(reponse);
+    }
+  };
+  const getFacuties = async () => {
+    const reponse: any = await apiService.getFaculties();
+    if (reponse) {
+      setDataFct(reponse);
+    }
+  };
   const FormItem = () => {
     return (
-      // <div className="flex justify-between content-center">
-      //   <div
-      //     onClick={() => inputRef.current.click()}
-      //     className="h-fit cursor-pointer flex flex-col items-center"
-      //   >
-      //     <img
-      //       src={`${API_URL}/images/${item.avatar}`}
-      //       alt="avatar"
-      //       className="object-cover"
-      //     />
-      //     {loading ? <LoadingOutlined /> : <PlusOutlined />}
-      //     <div style={{ marginTop: 20 }}>Tải Ảnh Lên</div>
-      //     <input
-      //       ref={inputRef}
-      //       style={{
-      //         display: 'none',
-      //       }}
-      //       onChange={handleChange}
-      //       accept="/image/*"
-      //     />
-      //   </div>
+      <div className="flex justify-between content-center">
+        <div
+          onClick={() => inputRef.current.click()}
+          className=" cursor-pointer "
+        >
+          {URLImage ? (
+            <>
+              <div className="w-[300px] flex flex-col items-center">
+                <img
+                  src={URLImage}
+                  alt="avatar"
+                  className="object-cover w-[300px] h-[300px] rounded-lg"
+                />
 
-      //   <div className="w-full pr-10 pl-10">
-      //     <FormInput
-      //       className="w-full"
-      //       name="fullName"
-      //       label="Họ Và Tên"
-      //       rules={[
-      //         {
-      //           required: true,
-      //           message: 'Vui Lòng Nhập Vào Họ Và Tên',
-      //         },
-      //       ]}
-      //     />
-      //     <FormInput
-      //       className="w-full"
-      //       name="code"
-      //       label="Mã"
-      //       rules={[
-      //         {
-      //           required: true,
-      //           message: 'Vui Lòng Nhập Vào Mã',
-      //         },
-      //       ]}
-      //     />
-      //     <FormInput
-      //       className="w-full"
-      //       name="phoneNumber"
-      //       label="Số Điện Thoại"
-      //       rules={[
-      //         {
-      //           required: true,
-      //           message: 'Vui Lòng Nhập Vào Số Điện Thoại',
-      //         },
-      //       ]}
-      //     />
-      //     <FormInput
-      //       className="w-full"
-      //       name="address"
-      //       label="Địa Chỉ"
-      //       rules={[
-      //         {
-      //           required: true,
-      //           message: 'Vui Lòng Nhập Vào Địa Chỉ',
-      //         },
-      //       ]}
-      //     />
-      //     <label className="text-start w-full mb-4 text-black font-bold font-customFont ">
-      //       Chức Vụ Của Bạn
-      //     </label>
-      //     <Form.Item
-      //       className="w-full "
-      //       name="positionId"
-      //       rules={[
-      //         {
-      //           required: true,
-      //           message: 'Vui Lòng Nhập Vào Chức Vụ',
-      //         },
-      //       ]}
-      //     >
-      //       <Select
-      //         className="font-bold"
-      //         showSearch
-      //         placeholder="Chọn Chức Vụ"
-      //         optionFilterProp="children"
-      //         filterOption={(input: any, option: any) =>
-      //           (option?.label ?? '')
-      //             .toLowerCase()
-      //             .includes(input.toLowerCase())
-      //         }
-      //         options={positons.map((item: any) => ({
-      //           value: item.positionId,
-      //           label: item.positionName,
-      //         }))}
-      //       />
-      //     </Form.Item>
-      //     <label className="text-start w-full mb-4 text-black font-bold font-customFont">
-      //       Phòng/Khoa
-      //     </label>
-      //     <Form.Item
-      //       className="w-full "
-      //       name="facultyId"
-      //       rules={[
-      //         {
-      //           required: true,
-      //           message: 'Vui Lòng Nhập Vào Phòng/Khoa',
-      //         },
-      //       ]}
-      //     >
-      //       <Select
-      //         className="font-bold"
-      //         showSearch
-      //         placeholder="Chọn Phòng/Khoa"
-      //         optionFilterProp="children"
-      //         filterOption={(input: any, option: any) =>
-      //           (option?.label ?? '')
-      //             .toLowerCase()
-      //             .includes(input.toLowerCase())
-      //         }
-      //         options={dataFct.map((item: any) => ({
-      //           value: item.facultyId,
-      //           label: item.facultyName,
-      //         }))}
-      //       />
-      //     </Form.Item>
-      //   </div>
-      // </div>
-      <div>
-        <input type="file" onChange={handelOnChange} />
+                <div style={{ marginTop: 20 }} className="flex items-center ">
+                  {loading ? <LoadingOutlined /> : <PlusOutlined />}{' '}
+                  <span className="ml-2">Tải Ảnh Lên </span>
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="w-[300px] flex flex-col items-center">
+              <img
+                src={`${API_URL}/images/${item.avatar}`}
+                alt="avatar"
+                className="object-cover w-[300px] h-[300px] rounded-lg"
+              />
+
+              <div style={{ marginTop: 20 }} className="flex items-center ">
+                {loading ? <LoadingOutlined /> : <PlusOutlined />}{' '}
+                <span className="ml-2">Tải Ảnh Lên </span>
+              </div>
+            </div>
+          )}
+        </div>
+        <input
+          ref={inputRef}
+          style={{
+            display: 'none',
+          }}
+          type="file"
+          onChange={handleChange}
+          accept="image/*"
+        />
+        <div className="w-full pr-10 pl-10">
+          <FormInput
+            className="w-full"
+            name="fullName"
+            label="Họ Và Tên"
+            rules={[
+              {
+                required: true,
+                message: 'Vui Lòng Nhập Vào Họ Và Tên',
+              },
+            ]}
+          />
+          <FormInput
+            className="w-full"
+            name="code"
+            label="Mã"
+            rules={[
+              {
+                required: true,
+                message: 'Vui Lòng Nhập Vào Mã',
+              },
+            ]}
+          />
+          <FormInput
+            className="w-full"
+            name="phoneNumber"
+            label="Số Điện Thoại"
+            rules={[
+              {
+                required: true,
+                message: 'Vui Lòng Nhập Vào Số Điện Thoại',
+              },
+            ]}
+          />
+          <FormInput
+            className="w-full"
+            name="address"
+            label="Địa Chỉ"
+            rules={[
+              {
+                required: true,
+                message: 'Vui Lòng Nhập Vào Địa Chỉ',
+              },
+            ]}
+          />
+          <label className="text-start w-full mb-4 text-black font-bold font-customFont ">
+            Chức Vụ Của Bạn
+          </label>
+          <Form.Item
+            className="w-full "
+            name="positionId"
+            rules={[
+              {
+                required: true,
+                message: 'Vui Lòng Nhập Vào Chức Vụ',
+              },
+            ]}
+          >
+            <Select
+              className="font-bold"
+              showSearch
+              placeholder="Chọn Chức Vụ"
+              optionFilterProp="children"
+              filterOption={(input: any, option: any) =>
+                (option?.label ?? '')
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
+              }
+              options={positons.map((item: any) => ({
+                value: item.positionId,
+                label: item.positionName,
+              }))}
+            />
+          </Form.Item>
+          <label className="text-start w-full mb-4 text-black font-bold font-customFont">
+            Phòng/Khoa
+          </label>
+          <Form.Item
+            className="w-full "
+            name="facultyId"
+            rules={[
+              {
+                required: true,
+                message: 'Vui Lòng Nhập Vào Phòng/Khoa',
+              },
+            ]}
+          >
+            <Select
+              className="font-bold"
+              showSearch
+              placeholder="Chọn Phòng/Khoa"
+              optionFilterProp="children"
+              filterOption={(input: any, option: any) =>
+                (option?.label ?? '')
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
+              }
+              options={dataFct.map((item: any) => ({
+                value: item.facultyId,
+                label: item.facultyName,
+              }))}
+            />
+          </Form.Item>
+        </div>
       </div>
     );
   };
@@ -244,7 +276,7 @@ export default function ModalProfile({
       dataItem={item}
       FormItem={<FormItem />}
       form={form}
-      width={900}
+      width={1000}
       handleOk={() => handelOk()}
     />
   );
