cb2d12875de13d413c0c13961790fe1c64f9263c
JK <vandung31141@gmail.com>
2023-05-26 23:15:34 +0700

    optimization

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/CP25Team02/',
-
+  // base: '/CP25Team02/',
   plugins: [react()],
 });
